6d5e11225a01ba299d02e52692ca7f9d9b6ce973
Tsutomu Kawamura <cognitom@gmail.com>
2016-07-11 16:42:31 +0900

    console.log() に細工して、テストから履歴を参照できるように

--- words/fecha/index.js
+++ words/fecha/index.js
@@ -1,4 +1,7 @@
 import fecha from 'fecha'
 
-const str = fecha.format(new Date(2016, 6, 7), 'YYYY/MM/DD')
-document.body.innerText = str
+const
+  d = new Date(2016, 6, 7),
+  str = fecha.format(d, 'YYYY/MM/DD')
+
+console.log(str) // 2016/06/07
--- words/fecha/spec.js
+++ words/fecha/spec.js
@@ -1,14 +1,15 @@
 import test from 'ava'
 import Nightmare from 'nightmare'
 
-const url = 'http://localhost:3030/fecha'
-
 test('gets the formated date', async function(t) {
-  const result = await Nightmare()
-    .goto(url)
-    .wait(() => document.body.innerText)
-    .evaluate(() => document.body.innerText)
-    .end()
+  const
+    url = 'http://localhost:3030/fecha',
+    history = await Nightmare()
+      .goto(url)
+      .wait()
+      .evaluate(() => console.history)
+      .end(),
+    log = history.pop()
 
-  t.is(result, '2016/07/07')
+  t.is(log, '2016/07/07')
 })
